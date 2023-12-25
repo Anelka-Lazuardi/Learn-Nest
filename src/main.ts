007ca0b250a14a0a5362e8d6ae6fd720de9d758d
anelka ganteng <alanelka42@gmail.com>
2023-12-25 23:49:16 +0700

    feat:add e2e test and config module

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
+  console.log(process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asd'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(5000);
 }
 bootstrap();
---
+++ src/setup-app.ts
@@ -0,0 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
+
+export const setupApp = (app: any) => {
+  app.use(
+    cookieSession({
+      keys: ['asd'],
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+};
